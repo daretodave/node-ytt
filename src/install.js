180e41d0ac0d52cdab80eb0e3584c50721b0ab8f
David Rehmat <me@dave.blue>
2021-11-09 23:44:19 -0500

    auto fetch latest cli tag - publish

--- src/install.js
+++ src/install.js
@@ -1,38 +1,49 @@
 const {type} = require("os");
 const {join} = require("path");
-const fetch = require("node-fetch");
+
 const fs = require("fs").promises;
+const fetch = require("node-fetch");
+
+const CLI_NAME="vmware-tanzu/carvel-ytt";
+
+install().catch(console.error);
 
 function install() {
-    const version = "0.37.0";
+    const releaseQueryUrl = `https://api.github.com/repos/${CLI_NAME}/releases/latest`;
 
-    const os = type().toLowerCase();
-
-    let platform = "linux";
-
-
-    if (os.includes("windows")) {
-        platform = "windows";
-    } else if (os.includes("darwin")) {
-        platform = "darwin";
-    }
-
-    const ext =  platform === "windows" ? ".exe" : "";
-    const arch = process.arch.includes("arm") ? "arm" : "amd64";
-    const url = releaseFor(version, platform, arch) + ext;
-    const cli = join("bin", "ytt-cli" + ext);
-
-    return fetch(url, {
+    return fetch(releaseQueryUrl, {
         method: 'GET'
-    }).then(res => res.buffer()).then(buffer => {
-        return fs.writeFile(cli, buffer)
-    }).then(() => {
-        return fs.chmod(cli, 0o755);
+    }).then(res => res.json()).then(json => {
+        const version = json.tag_name;
+        const os = type().toLowerCase();
+
+        let platform = "linux";
+
+        if (os.includes("windows")) {
+            platform = "windows";
+        } else if (os.includes("darwin")) {
+            platform = "darwin";
+        }
+
+        const ext =  platform === "windows" ? ".exe" : "";
+        const arch = process.arch.includes("arm") ? "arm" : "amd64";
+        const url = releaseFor(version, platform, arch) + ext;
+        const cli = join(__dirname, "..", "bin", "ytt-cli" + ext);
+
+        console.log(`Download ${version} for ${arch} from ${CLI_NAME} @ url ${url}`)
+
+        return fetch(url, {
+            method: 'GET'
+        }).then(res => res.buffer()).then(buffer => {
+            return fs.writeFile(cli, buffer)
+        }).then(() => {
+            return fs.chmod(cli, 0o755);
+        });
     });
+    
 }
 
 function releaseFor(version, platform, arch) {
-    return `https://github.com/vmware-tanzu/carvel-ytt/releases/download/v${version}/ytt-${platform}-${arch}`;
+    return `https://github.com/${CLI_NAME}/releases/download/${version}/ytt-${platform}-${arch}`;
 }
 
-install()
